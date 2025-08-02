67cc58ec4bcedc07591cd7d84d12cdc5f2cad4ea
castillo <castillo@192.168.36.119>
2025-08-02 17:04:17 -0600

    "cambios 02-08-2025"

---
+++ src/github/GITHUB.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package github;
+
+/**
+ *
+ * @author castillo
+ */
+public class GITHUB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String nombre, edad;
+        Scanner lector=new Scanner (System.in);
+        System.out.println("ingrese su nombre");   
+        nombre=lector.netx();
+        System.out.println("ingrese su edad");
+        edad=lector.next();
+    
+    }
+    
+}
